0e4fa28c128379833d8036f8929026148e36f01f
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-21 16:43:23 +0000

    Set up local storage for 8am and 9am

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ $('#currentDay').text(moment());
 $(".saveBtn").on("click", function() {
 
     var usersTask = $(this).siblings(".description").val();
-    console.log(usersTask)
+    console.log(usersTask);
     var timeTaskScheduled = $(this).parent().attr("id");
-    console.log(timeTaskScheduled)
+    console.log(timeTaskScheduled);
+
+    localStorage.setItem(timeTaskScheduled, usersTask);
 })
 
 
 
+var localStorage8am = localStorage.getItem("8"); 
+$("#8 .description").val(localStorage8am)
 
-
-
-
+var localStorage9am = localStorage.getItem("9"); 
+$("#9 .description").val(localStorage9am)
 
 })
