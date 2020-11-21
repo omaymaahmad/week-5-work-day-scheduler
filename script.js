94988213c933b6a40e942cd9093c974d3de43f79
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-21 16:55:10 +0000

    Add checks for time being past, present or future

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $("#currentDay").text(moment());
+  var currentWorkingHour = moment().hours();
 
   $(".saveBtn").on("click", function () {
     var usersTask = $(this).siblings(".description").val();
@@ -8,6 +9,8 @@ $(document).ready(function () {
     localStorage.setItem(timeTaskScheduled, usersTask);
   });
 
+
+
   // Getting any stored tasks for 8am - 9am from local storage
   var localStorage8am = localStorage.getItem("8");
   $("#8 .description").val(localStorage8am);
@@ -15,4 +18,22 @@ $(document).ready(function () {
   // Getting any stored tasks for 9am - 10am from local storage
   var localStorage9am = localStorage.getItem("9");
   $("#9 .description").val(localStorage9am);
+
+
+
+  function checkPastPresentFuture(){
+    $(".time-block").each(function(){
+        var hourForBlockBeingChecked = parseInt($(this).attr("id"));
+        
+        if (hourForBlockBeingChecked < currentWorkingHour){
+            $(this).addClass("past");
+        } else if (hourForBlockBeingChecked === currentWorkingHour){
+            $(this).addClass("present"); 
+        } else {
+            $(this).addClass("future"); 
+        }
+    })
+  }
+
+  checkPastPresentFuture();
 });
