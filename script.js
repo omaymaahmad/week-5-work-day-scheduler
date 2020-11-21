fbb5abf99f9490f90c2031ff375193f920d0079e
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-21 16:34:14 +0000

    Set up save button functionality

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-    
+
 $('#currentDay').text(moment());
 
+$("#saveBtn").on("click", function() {
 
-
-
+    var usersTask = $(this).siblings(".description").val();
+    console.log(usersTask)
+    var timeTaskScheduled = $(this).parent().attr("id");
+    console.log(timeTaskScheduled)
+})
 
 
 
