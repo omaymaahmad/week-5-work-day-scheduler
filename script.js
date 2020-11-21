e2d946775c41a2a86cc2bf799388b25db06e47fe
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-21 16:44:58 +0000

    remove console logs add comments and format

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#currentDay").text(moment());
 
-$('#currentDay').text(moment());
-
-$(".saveBtn").on("click", function() {
-
+  $(".saveBtn").on("click", function () {
     var usersTask = $(this).siblings(".description").val();
-    console.log(usersTask);
     var timeTaskScheduled = $(this).parent().attr("id");
-    console.log(timeTaskScheduled);
 
     localStorage.setItem(timeTaskScheduled, usersTask);
-})
+  });
 
+  // Getting any stored tasks for 8am - 9am from local storage
+  var localStorage8am = localStorage.getItem("8");
+  $("#8 .description").val(localStorage8am);
 
-
-var localStorage8am = localStorage.getItem("8"); 
-$("#8 .description").val(localStorage8am)
-
-var localStorage9am = localStorage.getItem("9"); 
-$("#9 .description").val(localStorage9am)
-
-})
+  // Getting any stored tasks for 9am - 10am from local storage
+  var localStorage9am = localStorage.getItem("9");
+  $("#9 .description").val(localStorage9am);
+});
